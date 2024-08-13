638d4dc2204ac7bc3722ea990e5915baeef6c8c0
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-13 14:51:02 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   defaultIconURL: '/assets/img/icon.png',
-  discordInvite: 'https://discord.gg/nH9D8Jg',
+  discordInvite: 'https://discord.gg/Eyd3c8APMq',
   recaptchaSiteKey: '6LfvCsEZAAAAAJjjEsBvG5PWnh_45rqCp_RuFZfi',
   endpoint: '/api/v1',
   production: true,
-  url: 'https://dlist.xyz',
-  version: 'v0.0.2a'
+  url: 'https://bump.prismbot.icu',
+  version: 'v0.0.1a'
 };
